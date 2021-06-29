2775e8eb459704be182d3319b329e7046768f7a2
quynh <quynhnguyentm9@gmail.com>
2021-06-29 18:51:24 +0700

    THTTW tuần 3

---
+++ bookmanager/Controllers/bookController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using bookmanager.Models;
+namespace bookmanager.Controllers
+{
+    public class bookController : Controller
+    {
+        // GET: book
+        BookManagerContext context = new BookManagerContext();
+        public ActionResult Listbook()
+        {
+            
+            var listbook = context.books.ToList();
+            return View(listbook);
+        }
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            
+            book Book = context.books.SingleOrDefault(p => p.Id == id);
+            if (Book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Book);
+        }
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection books)
+        {
+            var Book = new book();
+            
+            Book.Title = books["Title"];
+            Book.Author = books["Author"];
+            Book.Description = books["Description"];
+            Book.Images = books["Images"];
+            Book.Price = Int32.Parse(books["Price"]);
+            context.books.Add(Book);
+            context.SaveChanges();
+            return RedirectToAction("Listbook");
+            
+        }
+        [Authorize]
+        public ActionResult Edit(int id)
+        {
+            var Book = context.books.Find(id);
+            return View(Book);
+        }
+        [HttpPost]
+        public ActionResult Edit(FormCollection books,int id)
+        {
+            
+            var Book = context.books.Find(id);
+            Book.Title = books["Title"];
+            Book.Author = books["Author"];
+            Book.Description = books["Description"];
+            Book.Images = books["Images"];
+            Book.Price = Int32.Parse(books["Price"]);
+            context.SaveChanges();
+            return RedirectToAction("Listbook");
+        }
+        [Authorize]
+        public ActionResult Delete(int id)
+        {
+            book Book = context.books.Find(id);
+            return View(Book);
+        }
+        [HttpPost]
+        public ActionResult Deletebook(int id)
+        {
+            book Book = context.books.Find(id);
+            if (Book != null)
+            {
+                context.books.Remove(Book);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Listbook");
+        }
+    }
+}
---
+++ bookmanager/Models/book.cs
@@ -0,0 +1,34 @@
+﻿namespace bookmanager.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("book")]
+    public partial class book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage ="Vui long nhap tua de")]
+        [StringLength(100, ErrorMessage ="Tua de khong qua 100")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage ="Vui long nhap chi tiet")]
+        [StringLength(200, ErrorMessage ="Description khong qua 200")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Vui long nhap tac gia")]
+        [StringLength(30,ErrorMessage ="Tac gia khong qua 30")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Vui long nhap hinh anh")]
+        [StringLength(255)]
+        public string Images { get; set; }
+        [Required(ErrorMessage = "Vui long nhap gia")]
+        [Range(1000, 1000000, ErrorMessage = "Giá sách từ 1000 đến 1000000")]
+        public int Price { get; set; }
+    }
+}
